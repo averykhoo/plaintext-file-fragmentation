706e4a2b84f197baba2180f9c0c3ca3049f0be20
Avery Khoo <averykhoo@gmail.com>
2019-04-02 17:56:13 +0800

    code comments

--- frag_decode.py
+++ frag_decode.py
@@ -9,24 +9,37 @@ output_folder = os.path.join(this_folder, r'output_decoded')
 
 if __name__ == '__main__':
 
+    # create folder to place plaintext fragment files
     if not os.path.isdir(source_folder):
         assert not os.path.exists(source_folder)
         os.makedirs(source_folder)
         print('source folder <{}> does not exist, creating...'.format(source_folder))
 
-    if not os.path.isdir(output_folder):
-        assert not os.path.exists(output_folder)
-        os.makedirs(output_folder)
-        print('output folder <{}> does not exist, creating...'.format(output_folder))
+    # nothing to decode
+    if len(os.listdir(source_folder)) == 0:
+        print('nothing to decode, place files in <{}>'.format(source_folder))
 
-    for temp_archive_path in restore_files(source_folder, verbose=True):
-        if temp_archive_path is None:
-            continue
-        print('restored to <{}>, unpacking archive...'.format(temp_archive_path))
-        with tarfile.open(temp_archive_path, mode='r:gz') as tf:
-            tf.extractall(path=output_folder)
+    # start decoding
+    else:
+        if not os.path.isdir(output_folder):
+            assert not os.path.exists(output_folder)
+            os.makedirs(output_folder)
+            print('output folder <{}> does not exist, creating...'.format(output_folder))
 
-        print('unpacked <{}>, deleting archive...'.format(temp_archive_path))
-        os.remove(temp_archive_path)
+        # decode each bunch of fragments separately
+        for temp_archive_path in restore_files(source_folder, verbose=True):
+
+            # did not decode
+            if temp_archive_path is None:
+                continue
+
+            # unzip
+            print('restored to <{}>, unpacking archive...'.format(temp_archive_path))
+            with tarfile.open(temp_archive_path, mode='r:gz') as tf:
+                tf.extractall(path=output_folder)
+
+            # unpack and remove zip
+            print('unpacked <{}>, deleting archive...'.format(temp_archive_path))
+            os.remove(temp_archive_path)
 
     print('done!')
--- frag_encode.py
+++ frag_encode.py
@@ -9,36 +9,47 @@ source_folder = os.path.join(this_folder, r'input')
 output_folder = os.path.join(this_folder, r'ascii85_encoded')
 
 if __name__ == '__main__':
+
+    # create folder to place input files and folders
     if not os.path.isdir(source_folder):
         assert not os.path.exists(source_folder)
         os.makedirs(source_folder)
         print('source folder <{}> does not exist, creating...'.format(source_folder))
 
+    # nothing to encode
     if len(os.listdir(source_folder)) == 0:
         print('nothing to encode, place files in <{}>'.format(source_folder))
 
+    # start encoding everything
     else:
         if not os.path.isdir(output_folder):
             assert not os.path.exists(output_folder)
             os.makedirs(output_folder)
             print('output folder <{}> does not exist, creating...'.format(output_folder))
 
-        temp_archive_name = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
-        temp_archive_path = os.path.abspath(os.path.join(output_folder, temp_archive_name + '.tar.gz'))
+        # what to name the archive
+        archive_name = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
+        archive_path = os.path.abspath(os.path.join(output_folder, archive_name + '.tar.gz'))
 
-        if os.path.exists(temp_archive_path):
-            print('<{}> already exists, will work on existing file'.format(temp_archive_path))
+        # should never clash since we're using datetime
+        if os.path.exists(archive_path):
+            print('<{}> already exists, will remove...'.format(archive_path))
+            os.remove(archive_path)
 
-        else:
-            print('temporarily archiving <{}> to <{}>'.format(source_folder, temp_archive_path))
-            with tarfile.open(temp_archive_path, mode='w:gz') as tf:
-                tf.add(source_folder, arcname=temp_archive_name)
+        # archive everything into a gzip file
+        print('temporarily archiving <{}> to <{}>'.format(source_folder, archive_path))
+        with tarfile.open(archive_path, mode='w:gz') as tf:
+            tf.add(source_folder, arcname=archive_name)
 
-        print('fragmenting <{}> to <{}>'.format(temp_archive_path, output_folder))
-        fragment_paths = fragment_file(temp_archive_path, output_folder, verbose=True)
+        # plaintext fragmentation (size determined by defaults)
+        print('fragmenting <{}> to <{}>'.format(archive_path, output_folder))
+        fragment_paths = fragment_file(archive_path, output_folder, verbose=True)
 
-        print('deleting temp archive <{}>'.format(temp_archive_path))
-        os.remove(temp_archive_path)
+        # remove gzip file, archive input folder
+        print('deleting temp archive <{}>'.format(archive_path))
+        os.remove(archive_path)
+        os.rename(source_folder, archive_name)
+        os.makedirs(source_folder)
 
         print('created {} fragments'.format(len(fragment_paths)))
 
