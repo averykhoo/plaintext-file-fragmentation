252e24d8ff1d4c698e4623720e16bfb2f2a874a9
Avery Khoo <averykhoo@gmail.com>
2019-07-03 15:58:08 +0800

    improved doctests

--- frag_rc4.py
+++ frag_rc4.py
@@ -1,9 +1,12 @@
+import codecs
+from typing import List
 from typing import Union
 
 
 def rc4(key: Union[str, bytes, bytearray],
         input_bytes: Union[bytes, bytearray],
-        initialization_vector: Union[bytes, bytearray] = b'') -> bytearray:
+        initialization_vector: Union[bytes, bytearray] = b'',
+        ) -> bytearray:
     """
     single-function RC4-drop stream encryption
     uses IV to determine how much of keystream to skip
@@ -105,13 +108,13 @@ def rc4a_stream(key):
     key_length = len(key)
 
     j = 0
-    S1 = list(range(256))
+    S1 = list(range(256))  # type: List[int]
     for i in range(256):
         j = (j + S1[i] + key[i % key_length]) & 0xFF
         S1[i], S1[j] = S1[j], S1[i]
 
     j = 0
-    S2 = list(range(256))
+    S2 = list(range(256))  # type: List[int]
     for i in range(256):
         j = (j + S2[i] + key[i % key_length]) & 0xFF
         S2[i], S2[j] = S2[j], S2[i]
@@ -146,7 +149,16 @@ def rc4a_encode(key, input_bytes, initialization_vector=b''):
 
 
 class RC4(object):
-    def __init__(self, key, skip=None):
+    def __init__(self, key: Union[str, bytes, bytearray], skip=None):
+        """
+        >>> RC4('key').encode_str('test')
+        [127, 9, 71, 153]
+        >>> RC4('key').decode_str(RC4('key').encode_str('test'))
+        'test'
+
+        :param key:
+        :param skip:
+        """
         self.i = 0
         self.j = 0
         self.S = self.KSA(key)
@@ -221,6 +233,20 @@ class RC4(object):
 
 class RC4A(RC4):
     def __init__(self, key, skip=0):
+        """
+        >>> RC4A('key').encode_str('test')
+        [127, 110, 31, 24]
+        >>> RC4A('key').decode_str(RC4A('key').encode_str('test'))
+        'test'
+
+        >>> RC4A('key', 1234).encode_str('test')
+        [250, 235, 192, 199]
+        >>> RC4A('key', 1234).decode_str(RC4A('key', 1234).encode_str('test'))
+        'test'
+
+        :param key:
+        :param skip:
+        """
         super(RC4A, self).__init__(key)
         self.S2 = self.KSA(key)
         self.j2 = 0
@@ -257,6 +283,15 @@ class RC4A(RC4):
 
 class VMPC(RC4):
     def __init__(self, key, skip=0):
+        """
+        >>> VMPC('key').encode_str('test')
+        [19, 95, 153, 146]
+        >>> VMPC('key').decode_str(VMPC('key').encode_str('test'))
+        'test'
+
+        :param key:
+        :param skip:
+        """
         super(VMPC, self).__init__(key)
 
         if skip > 0:
@@ -282,6 +317,15 @@ class VMPC(RC4):
 
 class RCPlus(RC4):
     def __init__(self, key, skip=0):
+        """
+        >>> RCPlus('key').encode_str('test')
+        [39, 207, 224, 135]
+        >>> RCPlus('key').decode_str(RCPlus('key').encode_str('test'))
+        'test'
+
+        :param key:
+        :param skip:
+        """
         super(RCPlus, self).__init__(key)
 
         if skip > 0:
@@ -332,13 +376,82 @@ class RCDrop(RC4):
     """
 
     def __init__(self, key, skip=768):
+        """
+        >>> RCDrop('key', 4096).encode_str('test')
+        [101, 75, 195, 218]
+        >>> RCDrop('key', 4096).decode_str(RCDrop('key', 4096).encode_str('test'))
+        'test'
+
+        :param key:
+        :param skip:
+        """
         super(RCDrop, self).__init__(key)
 
         if skip > 0:
             self.PRGA(skip)
 
 
-def test():
+def _encrypt_to_hex(key, text):
+    """
+    # Test case 1
+    # key = 'Key' # '4B6579' in hex
+    # plaintext = 'Plaintext'
+    # ciphertext = 'BBF316E8D940AF0AD3'
+    >>> _encrypt_to_hex('Key', 'Plaintext')
+    'BBF316E8D940AF0AD3'
+
+    # Test case 2
+    # key = 'Wiki' # '57696b69'in hex
+    # plaintext = 'pedia'
+    # ciphertext = 1021BF0420
+    >>> _encrypt_to_hex('Wiki', 'pedia')
+    '1021BF0420'
+
+    # Test case 3
+    # key = 'Secret' # '536563726574' in hex
+    # plaintext = 'Attack at dawn'
+    # ciphertext = 45A01F645FC35B383552544B9BF5
+    >>> _encrypt_to_hex('Secret', 'Attack at dawn')
+    '45A01F645FC35B383552544B9BF5'
+
+    :param key:
+    :param plaintext:
+    :return:
+    """
+    return codecs.encode(bytes(rc4(key.encode('ascii'), text.encode('utf8'))), 'hex_codec').decode('ascii').upper()
+
+
+def _decrypt_from_hex(key, text):
+    """
+    # Test case 1
+    # key = 'Key' # '4B6579' in hex
+    # plaintext = 'Plaintext'
+    # ciphertext = 'BBF316E8D940AF0AD3'
+    >>> _decrypt_from_hex('Key', 'BBF316E8D940AF0AD3')
+    'Plaintext'
+
+    # Test case 2
+    # key = 'Wiki' # '57696b69'in hex
+    # plaintext = 'pedia'
+    # ciphertext = 1021BF0420
+    >>> _decrypt_from_hex('Wiki', '1021BF0420')
+    'pedia'
+
+    # Test case 3
+    # key = 'Secret' # '536563726574' in hex
+    # plaintext = 'Attack at dawn'
+    # ciphertext = 45A01F645FC35B383552544B9BF5
+    >>> _decrypt_from_hex('Secret', '45A01F645FC35B383552544B9BF5')
+    'Attack at dawn'
+
+    :param key:
+    :param plaintext:
+    :return:
+    """
+    return rc4(key.encode('ascii'), codecs.decode(text, 'hex_codec')).decode('utf8')
+
+
+if __name__ == '__main__':
     # does it crash
     RC4('key').PRGA(4096)
     RC4A('key').PRGA(4096)
@@ -346,71 +459,32 @@ def test():
     RCDrop('key', 4096).PRGA(4096)
     RCPlus('key').PRGA(4096)
 
-    import codecs
-
-    # def encrypt(key, plaintext):
-    #     return codecs.encode(bytes(RC4(key).encode_str(plaintext)), 'hex_codec').decode('ascii').upper()
-    #
-    # def decrypt(key, plaintext):
-    #     return RC4(key).decode_str(codecs.decode(plaintext, 'hex_codec'))
-
-    def encrypt(key, plaintext):
-        return codecs.encode(bytes(rc4(key.encode('ascii'), plaintext.encode('utf8'))), 'hex_codec').decode(
-            'ascii').upper()
-
-    def decrypt(key, plaintext):
-        return rc4(key.encode('ascii'), codecs.decode(plaintext, 'hex_codec')).decode('utf8')
-
-    # Test case 1
-    # key = '4B6579' # 'Key' in hex
-    # key = 'Key'
-    # plaintext = 'Plaintext'
-    # ciphertext = 'BBF316E8D940AF0AD3'
-    assert (encrypt('Key', 'Plaintext')) == 'BBF316E8D940AF0AD3', repr(encrypt('Key', 'Plaintext'))
-    assert (decrypt('Key', 'BBF316E8D940AF0AD3')) == 'Plaintext', repr(decrypt('Key', 'BBF316E8D940AF0AD3'))
-
-    # Test case 2
-    # key = 'Wiki' # '57696b69'in hex
-    # plaintext = 'pedia'
-    # ciphertext should be 1021BF0420
-    assert (encrypt('Wiki', 'pedia')) == '1021BF0420'
-    assert (decrypt('Wiki', '1021BF0420')) == 'pedia'
-
-    # Test case 3
-    # key = 'Secret' # '536563726574' in hex
-    # plaintext = 'Attack at dawn'
-    # ciphertext should be 45A01F645FC35B383552544B9BF5
-    assert (encrypt('Secret', 'Attack at dawn')) == '45A01F645FC35B383552544B9BF5'
-    assert (decrypt('Secret', '45A01F645FC35B383552544B9BF5')) == 'Attack at dawn'
-
-
-if __name__ == '__main__':
-    test()
-
     print('RC4', RC4('key').encode_str('test'))
-    print(RC4('key').decode_str(RC4('key').encode_str('test')))
     # [127, 9, 71, 153]
+    print(RC4('key').decode_str(RC4('key').encode_str('test')))
     # test
 
     print('RC4A', RC4A('key').encode_str('test'))
-    print(RC4A('key').decode_str(RC4A('key').encode_str('test')))
     # [127, 110, 31, 24]
+    print(RC4A('key').decode_str(RC4A('key').encode_str('test')))
     # test
 
     print('RC4A-drop', RC4A('key', 1234).encode_str('test'))
+    # [250, 235, 192, 199]
     print(RC4A('key', 1234).decode_str(RC4A('key', 1234).encode_str('test')))
+    # test
 
     print('VMPC', VMPC('key').encode_str('test'))
-    print(VMPC('key').decode_str(VMPC('key').encode_str('test')))
     # [19, 95, 153, 146]
+    print(VMPC('key').decode_str(VMPC('key').encode_str('test')))
     # test
 
     print('RCDrop', RCDrop('key', 4096).encode_str('test'))
-    print(RCDrop('key', 4096).decode_str(RCDrop('key', 4096).encode_str('test')))
     # [101, 75, 195, 218]
+    print(RCDrop('key', 4096).decode_str(RCDrop('key', 4096).encode_str('test')))
     # test
 
     print('RCPlus', RCPlus('key').encode_str('test'))
-    print(RCPlus('key').decode_str(RCPlus('key').encode_str('test')))
     # [39, 207, 224, 135]
+    print(RCPlus('key').decode_str(RCPlus('key').encode_str('test')))
     # test
