dd46026e3914c9f339a1aa0b0045f829869c947b
Avery Khoo <averykhoo@gmail.com>
2019-07-04 13:51:14 +0800

    update convenience bat file

--- decode.bat
+++ decode.bat
@@ -1,7 +1,5 @@
 :: SET LOCAL_PY_DIR=%LOCALAPPDATA%\Continuum\anaconda3
 SET LOCAL_PY_DIR=%HOMEPATH%\Anaconda3
-:: SET LOCAL_PY_DIR=C:\WinPython-64bit-2.7.10.3\python-2.7.10.amd64
 
 %LOCAL_PY_DIR%\python frag_decode.py
-pause
-::timeout 3
+timeout 3
